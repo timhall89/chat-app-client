abff9a75770ac456522908799b9be1498df1c688
Tim Hall <timhall@Tims-16-MacBook-Pro.local>
2020-10-26 11:08:18 +0000

    making app work in a basic way

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,44 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect, useCallback } from 'react';
+import io from "socket.io-client"
+import "./App.css"
+
+const socket = io('localhost:3001')
 
 function App() {
+  const [messages, setMessages] = useState<string[]>([]);
+  const appendMessage = (message: string) => setMessages(currentMessages => [...currentMessages, message]);
+  const [newMessage, setNewMessage] = useState('');
+
+  const onSubmit: React.FormEventHandler = e => {
+    appendMessage(newMessage);
+    socket.emit('chat message', newMessage);
+    setNewMessage('');
+    e.preventDefault();
+  }
+  useEffect(() => {
+    socket.on('chat message', (msg: string) => {
+      appendMessage(msg);
+    });
+  }, [])
+
+  const onNewMessageChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewMessage(e.target.value);
+  }, []);
+
   return (
     <div className="app">
       <header className="app-header">
         Chatty
       </header>
       <main className="main-grid">
-        <div className="message-list">
-
+        <div className="message-list-wrapper">
+          <ul className="message-list">
+            {messages.map(message => <li className="message">{message}</li>)}
+          </ul>
         </div>
-        <form>
-          <input id="m" /><button>Send</button>
+        <form onSubmit={onSubmit} className="message-form">
+          <input value={newMessage} id="m" autoComplete="off" name="newMessage" onChange={onNewMessageChange} />
+          <button type="submit">Send</button>
         </form>
       </main>
     </div>
